fb532b380b56f5868710c35d5951a973fd996568
Biozid Bostami <biozid.bostami@synesisit.com.bd>
2019-06-26 12:39:10 +0600

    [Add] configuration for cross origin

--- src/main/java/com/bio/demo/Controller/EmployeeController.java
+++ src/main/java/com/bio/demo/Controller/EmployeeController.java
@@ -12,6 +12,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("employee")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class EmployeeController {
     private final EmployeeService employeeService;
